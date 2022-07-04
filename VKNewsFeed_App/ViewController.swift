79e0fb600477e949c30bbe7c87d9a1b93a36586a
Felix Titov <titow.feliks@yandex.ru>
2022-07-04 23:48:13 +0300

    Initial Commit

---
+++ VKNewsFeed_App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  VKNewsFeed_App
+//
+//  Created by Felix Titov on 7/4/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
